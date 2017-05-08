0503529cfc715c1e35a450ae7ae13cc019bf7f40
larmas <lucas.armas.7@gmail.com>
2017-05-08 16:57:52 -0300

    Backlog implemantation

--- Backlog.java
+++ Backlog.java
@@ -1,15 +1,40 @@
-public class Backlog{
-	
-	public static void main(String[] args){
-		Actividades act = new Actividades(10,"contacto","deberia tener mail y numero de contacto",2,1);
-		/*act.durSprint=10;
-		act.funcionalidad= "contacto";
-		act.descripcion="deberia tener mail y numero de contacto";
-		act.prioridad=2;
-		act.costo=1;*/
-		System.out.println(act.costo);
+import java.util.ArrayList;
+import java.lang.RuntimeException;
 
+public class Backlog{
+	public ArrayList<Actividad> backlog;
+	public ArrayList<Actividad> sprint;
+	public int capacitySprint;
+
+	public Backlog(int capacity){
+		backlog = new ArrayList<Actividad>();
+		sprint = new ArrayList<Actividad>();
+		capacitySprint = capacity;
 	}
 
+	public Actividad getMaxPriorityBacklog(int capacity)throws RuntimeException{ //Capacity es el espacio que queda por llenar en el sprint
+		int index = 0;
+		while((backlog.get(index).costo > capacity)&&(index<backlog.size())){
+			index++;
+		}
+		if( index++ < backlog.size()){
+			Actividad aux = backlog.get(index);
+			backlog.remove(index);
+			return aux;
+		}else{
+			throw new RuntimeException("No hay posibles actividades");
+		}
+	}
 
+	public void insertBacklog(Actividad nact){
+		int index=0;
+		while((nact.prioridad < backlog.get(index).prioridad)&&(index<backlog.size())){
+				index++;
+		}
+		while((nact.costo > backlog.get(index).costo)&&(index < backlog.size())){
+			index++;
+		}
+		backlog.add(index,nact);
+	}
+	
 }
