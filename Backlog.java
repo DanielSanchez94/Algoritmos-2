648ef65e9ee373e3ca28a4998358165ca8e75d80
Daniel Sanchez <Daniel05071994@gmail.com>
2017-05-08 12:34:55 -0300

    agrego clases basicas

---
+++ Backlog.java
@@ -0,0 +1,15 @@
+public class Backlog{
+	
+	public static void main(String[] args){
+		Actividades act = new Actividades(10,"contacto","deberia tener mail y numero de contacto",2,1);
+		/*act.durSprint=10;
+		act.funcionalidad= "contacto";
+		act.descripcion="deberia tener mail y numero de contacto";
+		act.prioridad=2;
+		act.costo=1;*/
+		System.out.println(act.costo);
+
+	}
+
+
+}
