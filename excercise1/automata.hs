dd600302070b52a8a700bfc447aeab29873e0bd4
larmas <lucas.armas.7@gmail.com>
2017-05-24 19:57:12 -0300

    Possibly working, yeahhhh

--- excercise1/automata.hs
+++ excercise1/automata.hs
@@ -6,15 +6,28 @@ sublist [] = [[]]
 sublist (x:xs) = [(x:ys) | ys<-sublist xs] ++ sublist xs
 
 -- De una lista de listas elimina las listas que no son funcionales y totales
-repOkList :: Eq a => Eq b => [[(a,b)]] -> [[(a,b)]]
-repOkList [[]] = [[]]
-repOkList (x:xs)  | repOk x = x : repOkList xs
-                  | otherwise = repOkList xs
-                  where repOk [] = True
-                        repOk [x] = True
-                        repOk (x:y:xs)|fst x == fst y && snd x /= snd y = False
-                                      |fst x == fst y && snd x == snd y = False
-                                      |otherwise = True && repOk (y:xs)
+-- lista de estados - alfabeto - lista de deltas - lista de deltas filtrada
+repOkList :: [Int] -> [Char] -> [[((Int,Char),Int)]] -> [[((Int,Char),Int)]]
+repOkList ys zs [x] | functional x && total [(s,c) | s<-ys, c<-zs] x = [x]
+                    | otherwise = []
+repOkList ys zs (x:xs)  | functional x && total [(s,c) | s<-ys, c<-zs] x = x : repOkList ys zs xs
+                        | otherwise = repOkList ys zs xs
+
+-- Verifica si una lista delta es funcional
+-- un delta - true si es funcional
+functional :: [((Int,Char),Int)] -> Bool
+functional [] = True
+functional [x] = True
+functional (x:y:xs)  |fst x == fst y && snd x /= snd y = False
+                |fst x == fst y && snd x == snd y = False
+                |otherwise = True && functional (y:xs)
+
+-- Verifica si una lista delta es total
+-- un delta - true si es total
+total ::[(Int,Char)] -> [((Int,Char),Int)] -> Bool
+total [] _ = True
+total (x:xs) ys = elem x (reduc ys) && total xs ys
+              where reduc ts = [fst t | t <- ts]
 
 --Elimina los elementos repetido de una lista
 removeDups :: Eq a => [a] -> [a]
@@ -44,10 +57,11 @@ funFromTuples :: Eq a =>[(a,b)] -> a -> b
 funFromTuples ts x = fromJust (lookup x ts)
 
 -- Evalua si un DFA acepta un grupo de palabras y no acepta otro grupo
+-- automata - palabras pos - palabras neg - true si funciona
 evalString :: DFA Int -> [String] -> [String] ->Bool
 evalString dfa [] [] = True
-evalString dfa [] [y] = not (evalDFA dfa y)
-evalString dfa [x] [] = evalDFA dfa x
+evalString dfa (x:xs) [] = evalDFA dfa x && evalString dfa xs []
+evalString dfa [] (y:ys) = not (evalDFA dfa y) && evalString dfa [] ys
 evalString dfa (x:xs) (y:ys) = (evalDFA dfa x) && (not (evalDFA dfa y)) && evalString dfa xs ys
 
 -- Genera una lista de automatas variando el delta y el conjunto de estados finales
@@ -58,34 +72,33 @@ genDFA xs ys zs = [(xs, ys, (\s c -> funFromTuples d (s,c)), 0, (\s -> elem s z)
 -- Devuelve una lista de listas, es decir, una lista de deltas
 -- lista de estados - alfabeto - lista de lista de tuplas (a,b) donde a es una tupla (estado, caracter) y b un estado
 generateDelta :: [Int] -> [Char] -> [[((Int,Char),Int)]]
-generateDelta xs ys = removeDups (repOkList (sublist [((x,y),z) | x <- xs, y <- ys, z <- xs]))
+generateDelta xs ys = removeDups (repOkList xs ys (init (sublist [((x,y),z) | x <- xs, y <- ys, z <- xs])))
 
 -- Genera automatas a partir de 1 estado a k estados y se queda con el primero que acepte pos y rechase neg
 -- limite de estados k - contador - alfabeto - lista de automatas - palabras pos - palabras neg - automata
 auxFunction :: Int -> Int -> [Char] -> [DFA Int] -> [String] -> [String] -> DFA Int
-auxFunction k n alfab [] pos neg | n<k = auxFunction k (n+1) alfab (genDFA [x | x <- [0..n-1]] alfab (generateDelta [x | x <- [0..n-1]] alfab)) pos neg
+auxFunction k n alfab [] pos neg | n<=k = auxFunction k (n+1) alfab (genDFA [x | x <- [0..n-1]] alfab (generateDelta [x | x <- [0..n-1]] alfab)) pos neg
                                  | otherwise = error "No es posible formar un automata"
 auxFunction k n alfab (x:xs) pos neg | evalString x pos neg = x
                                      | otherwise = auxFunction k n alfab xs pos neg
 
 -- Funcion principal, utilizada para inicializar a la funcion auxFunction
 -- limite de estados k - alfabeto - palabras pos - palabras neg - (lista de estados,alfbeto, lista delta, estado inicial)
-mainFunction :: Int -> [Char] -> [String] -> [String] -> ([Int],[Char],[((Int, Char),Int)],Int)
+mainFunction :: Int -> [Char] -> [String] -> [String] -> ([Int],[Char],[((Int, Char),Int)],Int,[Int])
 mainFunction 0 _ _ _ = error "No se puede formar DFA con 0 estados"
 mainFunction k alfab pos neg | intersec pos neg = error "No es posible encontrar automata que reconozca y no reconozca una palabra"
                              | otherwise = printDFA (auxFunction k 2 alfab (genDFA [0] alfab (generateDelta [0] alfab)) pos neg)
 
 -- Imprime un automata con la delta en formato de lista (Aun no se muestran los estados finales)
 -- automata - (lista de estados,alfbeto, lista delta, estado inicial)
-printDFA :: DFA Int -> ([Int],[Char],[((Int, Char),Int)],Int)
-printDFA (qs,sigma,delta,s,inF) = (qs,sigma,getDelta qs sigma delta,s)
+printDFA :: DFA Int -> ([Int],[Char],[((Int, Char),Int)],Int,[Int])
+printDFA (qs,sigma,delta,s,inF) = (qs,sigma,getDelta qs sigma delta,s, getFinals qs inF)
 
 -- Funcion para recuperar la lista delta desde funFromTuples
 -- lista de estados - alfabeto - funcion delta - delta en forma de lista
 getDelta :: [Int] -> [Char] -> (Int->Char->Int) -> [((Int,Char),Int)]
 getDelta qs sigma delta = [((q,c),delta q c) | q<-qs, c<-sigma ]
 
--- Si mainFunction devolviera ([Int],[Char],[((Int, Char),Int)],Int) y en lugar de hacer
--- otherwise = auxFunction k 1 alfab (genDFA [0] alfab (generateDelta [0] alfab)) pos neg
--- hago  otherwise = printDFA (auxFunction k 1 alfab (genDFA [0] alfab (generateDelta [0] alfab)) pos neg)
--- tira el error:    *** Exception: Maybe.fromJust: Nothing
+--Funcion para recuperar el conjunto de estados finales
+getFinals :: [Int] -> (Int -> Bool) -> [Int]
+getFinals qs inF = [q | q<-qs, inF q]
